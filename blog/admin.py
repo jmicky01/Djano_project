ae8cb5f75ce249252fbb91343295133882148e9d
jmicky01 <kondreddy_jaipal@outlook.com>
2017-07-19 15:33:52 -0400

    Added blog content to collab_tools

---
+++ blog/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import CompanyName
+
+# Register your models here.
+
+admin.site.register(CompanyName)
---
+++ env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/jaipalkondreddy/git/Djano_project/env/bin/python3.6
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
